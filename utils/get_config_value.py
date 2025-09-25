3361719a2fca4453578aac312f42e095335dc3ab
yuichikondo09 <222544816+yuichikondo09@users.noreply.github.com>
2025-09-25 16:05:00 +0900

    Delete utils directory

--- utils/get_config_value.py
+++
@@ -1,5 +0,0 @@
-def get_config_value(config, key):
-    value = config.get(key)
-    if value is None:
-        raise ValueError(f"The '{key}' parameter is missing in the config.")
-    return value
